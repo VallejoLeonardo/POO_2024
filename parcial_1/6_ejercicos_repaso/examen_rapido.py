535142330622c08afc3a7dec72bc8bbec746a534
VallejoLeonardo <leonardo_3041230313@utd.edu.mx>
2024-05-24 08:04:11 -0600

    mayo 2024

--- parcial_1/6_ejercicos_repaso/examen_rapido.py
+++ parcial_1/6_ejercicos_repaso/examen_rapido.py
@@ -11,17 +11,17 @@ while respuesta == "Si":
     cal_PF = input("Ingrese la calificación del proyecto final: ")
     promedio = (float(cal1) + float(cal2) + float(cal3) + float(cal_PF)) / 4
 
-    # a) Calcular e imprimir el promedio de los parciales
+    # a Calcular e imprimir el promedio de los parciales
     print("El promedio de los parciales es:", promedio)
 
-    # b) Calcular e imprimir la calificación final 
+    # b Calcular e imprimir la calificación final 
     calificacion_final = (promedio + float(cal_PF)) / 2
     if calificacion_final < 80 and float(cal_PF) > 50:
         print("Calificación final:", calificacion_final, "- Presenta examen extraordinario")
     else:
         print("Calificación final:", calificacion_final)
 
-    # c) Calcular el promedio de la calificación final de los alumnos capturados
+    # c Calcular el promedio de la calificación final de los alumnos capturados
     suma_calificaciones_finales += calificacion_final
     contador += 1
 
