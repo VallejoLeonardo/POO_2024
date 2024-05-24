c810a4170365460506689e42ca3fcb49368387fc
VallejoLeonardo <leonardo_3041230313@utd.edu.mx>
2024-05-24 08:12:45 -0600

    prueba

--- parcial_1/6_ejercicos_repaso/examen_rapido.py
+++ parcial_1/6_ejercicos_repaso/examen_rapido.py
@@ -30,3 +30,5 @@ while respuesta == "Si":
 promedio_calificaciones_finales = suma_calificaciones_finales / contador
 print("El promedio de la calificación final de los alumnos capturados es:", promedio_calificaciones_finales)
 
+print("Hola")
+
